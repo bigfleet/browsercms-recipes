a59fd71b5ed27d6bae52a9d10e259eb41fa336bf
Jim Van Fleet <jim@jimvanfleet.com>
2009-07-14 16:40:15 -0400

    It's actually another cookbook altogether now

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,8 @@
 name    "broswercms"
 description 'Run a browsercms instance on this node'
-depends "apache2::mod_rails"
+depends "passenger_apache2::mod_rails"
 depends "apt"
 depends "gems"
 depends "git"
-depends "passenger"
 maintainer 'Jim Van Fleet'
 maintainer_email 'jvanfleet@tradeking.com'
