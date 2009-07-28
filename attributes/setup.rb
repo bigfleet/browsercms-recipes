399ad82b946db4e0babdacb486cc8b278ffa0dda
Jim Van Fleet <jim@jimvanfleet.com>
2009-07-28 11:01:59 -0400

    Changing name of key to create database

--- attributes/setup.rb
+++ attributes/setup.rb
@@ -2,8 +2,9 @@ db_password = ""
 chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
 20.times { |i| db_password << chars[rand(chars.size-1)] }
 
+db_apps Mash.new unless attribute?("db_apps")
 railsapps Mash.new unless attribute?("railsapps")
-(railsapps[:names]                       ||= []) << "browsercms"
+(db_apps[:names]                              ||= []) << "browsercms"
 railsapps[:browsercms]                        = Mash.new        unless railsapps.has_key?(:browsercms)
 railsapps[:browsercms][:db]                   = Mash.new        unless railsapps[:browsercms].has_key?(:db)
 railsapps[:browsercms][:db][:user]            = "browsercms_db" unless railsapps[:browsercms][:db].has_key?(:user)
